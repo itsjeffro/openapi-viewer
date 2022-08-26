b478332667eca8a719a03f7ff0817847714cf8a6
Jeff Nielsen <jeff.nielsen@elmosoftware.com.au>
2022-08-26 20:36:51 +1000

    Allow schema/example change, allow status change

--- src/components/ResponseList.tsx
+++ src/components/ResponseList.tsx
@@ -1,12 +1,13 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import schemaParser from "../lib/schemaParser";
+import {useState} from "react";
 
-const ResponseList = ({ responseBody }) => {
+const ResponseList = ({ responseBody }: any) => {
   const style = {
     background: '#f6f8fa',
     margin: 0,
     padding: '15px',
-    fontSize: '.75rem',
+    fontSize: '.85rem',
     fontFamily: 'monospace',
     lineHeight: '.75rem'
   };
@@ -14,21 +15,78 @@ const ResponseList = ({ responseBody }) => {
   const responses = Object.keys(responseBody.responses).map((httpCode: string) => {
     return {
       httpCode: httpCode,
-      ...responseBody.responses[httpCode].content['application/json'].schema,
+      schema: responseBody.responses[httpCode].content['application/json'].schema,
     }
   })
 
-  const firstExample = responses[0];
+  const responseHttpCodes = responses.map((response) => response.httpCode)
 
-  const parsedExample = schemaParser(firstExample);
+  const [selectedHttpCode, setSelectedHttpCode] = useState(responseHttpCodes[0])
+  const [selectedResponseTab, setSelectedResponseTab] = useState('example')
 
   return (
     <div className="endpoint-details__response">
       <div className="card">
-        <div className="card__header">Status: { firstExample.httpCode }</div>
-        <SyntaxHighlighter language="json" customStyle={ style }>
-          { JSON.stringify(parsedExample, null, 4) }
-        </SyntaxHighlighter>
+        <div className="card__header card__nav">
+          <button
+            className={ `card__nav-tab ${selectedResponseTab === 'example' ? 'active' : ''}` }
+            onClick={ () => setSelectedResponseTab('example') }
+          >Example response</button>
+          <button
+            className={ `card__nav-tab ${selectedResponseTab !== 'example' ? 'active' : ''}` }
+            onClick={ () => setSelectedResponseTab('schema') }
+          >Response schema</button>
+        </div>
+
+        <div className="card__header">
+          <span className="label__http-code">Status:</span>
+          <select
+            className="form-input"
+            onChange={ (e) => setSelectedHttpCode(e.target.value) }
+          >
+            { responseHttpCodes.map((responseHttpCode) => (
+              <option key={ responseHttpCode } value={ responseHttpCode }>{ responseHttpCode }</option>
+            )) }
+          </select>
+        </div>
+
+        <div
+          style={{ display: selectedResponseTab !== 'example' ? 'block' : 'none' }}
+        >
+          { responses.map((response) => {
+            const display = selectedHttpCode === response.httpCode ? 'block' : 'none';
+
+            return (
+              <div
+                key={ `schema-${response.httpCode}` }
+                style={ { display: display } }
+              >
+                <SyntaxHighlighter language="json" customStyle={ style }>
+                  { JSON.stringify(response.schema, null, 4) }
+                </SyntaxHighlighter>
+              </div>
+            )
+          }) }
+        </div>
+
+        <div
+          style={{ display: selectedResponseTab === 'example' ? 'block' : 'none' }}
+        >
+          { responses.map((response) => {
+            const display = selectedHttpCode === response.httpCode ? 'block' : 'none';
+
+            return (
+              <div
+                key={ `section-${response.httpCode}` }
+                style={ { display: display } }
+              >
+                <SyntaxHighlighter language="json" customStyle={ style }>
+                  { JSON.stringify(schemaParser(response.schema), null, 4) }
+                </SyntaxHighlighter>
+              </div>
+            )
+          }) }
+        </div>
       </div>
     </div>
   )
