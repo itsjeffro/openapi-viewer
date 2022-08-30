8f086821ae9e4d51aa7671736a117554a72aee71
Jeff Nielsen <jeff.nielsen@elmosoftware.com.au>
2022-08-30 20:44:01 +1000

    Use dta for open api data

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,6 +4,7 @@ import {StateContext} from "../state/stateProvider";
 import Container from "../components/Container";
 import Section from "../components/Section";
 import Header from "../components/Header";
+import OpenApi from "../lib/OpenApi";
 
 const HomeScreen = () => {
   const { state } = useContext(StateContext);
@@ -18,26 +19,28 @@ const HomeScreen = () => {
     )
   }
 
-  const openApi = state.openApi.data;
+  const openApi = new OpenApi(state.openApi.data);
+
+  const servers = openApi.servers().get();
 
   return (
     <>
       <Header>
-        <h1>{ openApi.info.title }</h1>
+        <h1>{ openApi.info().title }</h1>
       </Header>
 
       <Container>
         <Section>
           <h2>Introduction</h2>
 
-          <p>{ openApi.info.description }</p>
+          <p>{ openApi.info().description }</p>
         </Section>
 
         <Section>
           <h2>Servers</h2>
 
           <ul>
-            { openApi.servers.map((server: any, index: number) => (
+            { servers.map((server: any, index: number) => (
               <li key={ `server-${index}` }>
                 { server.url }
               </li>
