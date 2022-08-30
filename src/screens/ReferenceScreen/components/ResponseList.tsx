fe0f8d1a2d3f819df0e699bc9890fa5433c9e8df
Jeff Nielsen <jeff.nielsen@elmosoftware.com.au>
2022-08-30 20:44:55 +1000

    Fixed indentation and removed nesting of elements

--- src/screens/ReferenceScreen/components/ResponseList.tsx
+++ src/screens/ReferenceScreen/components/ResponseList.tsx
@@ -4,9 +4,9 @@ import {useState} from "react";
 import {Card, CardHeader} from '../../../components/Card'
 import * as styles from "../../../styles";
 
-const ResponseList = ({ responseBody }: any) => {
-  const responses = Object.keys(responseBody.responses).map((httpCode: string) => {
-    const status = responseBody.responses[httpCode];
+const ResponseList = ({ responses }: any) => {
+   responses = Object.keys(responses).map((httpCode: string) => {
+    const status = responses[httpCode];
 
     return {
       httpCode: httpCode,
@@ -15,86 +15,80 @@ const ResponseList = ({ responseBody }: any) => {
     }
   })
 
-  const responseHttpCodes = responses.map((response) => response.httpCode)
+  const responseHttpCodes = responses.map((response: any) => response.httpCode)
 
   const [selectedHttpCode, setSelectedHttpCode] = useState(responseHttpCodes[0])
   const [selectedResponseTab, setSelectedResponseTab] = useState('example')
 
   return (
-    <>
-      <h5>Responses</h5>
-
-      <div className="endpoint-details__response">
-        <Card>
-          <div className="card__header card__nav">
-            <button
-              className={ `card__nav-tab ${selectedResponseTab === 'example' ? 'active' : ''}` }
-              onClick={ () => setSelectedResponseTab('example') }
-            >Example response</button>
-            <button
-              className={ `card__nav-tab ${selectedResponseTab !== 'example' ? 'active' : ''}` }
-              onClick={ () => setSelectedResponseTab('schema') }
-            >Response schema</button>
-          </div>
-
-          <CardHeader>
-            <span className="label__http-code">Status:</span>
-            <select
-              className="form-input"
-              onChange={ (e) => setSelectedHttpCode(e.target.value) }
-            >
-              { responseHttpCodes.map((responseHttpCode) => (
-                <option key={ responseHttpCode } value={ responseHttpCode }>{ responseHttpCode }</option>
-              )) }
-            </select>
-          </CardHeader>
-
-          <div
-            style={{ display: selectedResponseTab !== 'example' ? 'block' : 'none' }}
-          >
-            { responses.map((response) => {
-              const display = selectedHttpCode === response.httpCode ? 'block' : 'none';
-
-              return (
-                <div
-                  key={ `schema-${response.httpCode}` }
-                  style={ { display: display } }
-                >
-                  <SyntaxHighlighter language="json" customStyle={ styles.highlighter }>
-                    { JSON.stringify(response.schema, null, 4) }
-                  </SyntaxHighlighter>
-                </div>
-              )
-            }) }
-          </div>
-
-          <div
-            style={{ display: selectedResponseTab === 'example' ? 'block' : 'none' }}
-          >
-            { responses.map((response) => {
-              const display = selectedHttpCode === response.httpCode ? 'block' : 'none';
-
-              let example = schemaParser(response.schema || {});
-
-              if (!example) {
-                example = response.description
-              }
-
-              return (
-                <div
-                  key={ `section-${response.httpCode}` }
-                  style={ { display: display } }
-                >
-                  <SyntaxHighlighter language="json" customStyle={ styles.highlighter }>
-                    { JSON.stringify(example, null, 4) }
-                  </SyntaxHighlighter>
-                </div>
-              )
-            }) }
-          </div>
-        </Card>
+    <Card>
+      <div className="card__header card__nav">
+        <button
+          className={ `card__nav-tab ${selectedResponseTab === 'example' ? 'active' : ''}` }
+          onClick={ () => setSelectedResponseTab('example') }
+        >Example response</button>
+        <button
+          className={ `card__nav-tab ${selectedResponseTab !== 'example' ? 'active' : ''}` }
+          onClick={ () => setSelectedResponseTab('schema') }
+        >Response schema</button>
       </div>
-    </>
+
+      <CardHeader>
+        <span className="label__http-code">Status:</span>
+        <select
+          className="form-input"
+          onChange={ (e) => setSelectedHttpCode(e.target.value) }
+        >
+          { responseHttpCodes.map((responseHttpCode: string) => (
+            <option key={ responseHttpCode } value={ responseHttpCode }>{ responseHttpCode }</option>
+          )) }
+        </select>
+      </CardHeader>
+
+      <div
+        style={{ display: selectedResponseTab !== 'example' ? 'block' : 'none' }}
+      >
+        { responses.map((response: any) => {
+          const display = selectedHttpCode === response.httpCode ? 'block' : 'none';
+
+          return (
+            <div
+              key={ `schema-${response.httpCode}` }
+              style={ { display: display } }
+            >
+              <SyntaxHighlighter language="json" customStyle={ styles.highlighter }>
+                { JSON.stringify(response.schema, null, 4) }
+              </SyntaxHighlighter>
+            </div>
+          )
+        }) }
+      </div>
+
+      <div
+        style={{ display: selectedResponseTab === 'example' ? 'block' : 'none' }}
+      >
+        { responses.map((response: any) => {
+          const display = selectedHttpCode === response.httpCode ? 'block' : 'none';
+
+          let example = schemaParser(response.schema || {});
+
+          if (!example) {
+            example = response.description
+          }
+
+          return (
+            <div
+              key={ `section-${response.httpCode}` }
+              style={ { display: display } }
+            >
+              <SyntaxHighlighter language="json" customStyle={ styles.highlighter }>
+                { JSON.stringify(example, null, 4) }
+              </SyntaxHighlighter>
+            </div>
+          )
+        }) }
+      </div>
+    </Card>
   )
 }
 
