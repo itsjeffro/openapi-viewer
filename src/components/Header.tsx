1993526037980d29321ba0eb490c3806d5f935fb
Jeff Nielsen <jeff.nielsen@elmosoftware.com.au>
2022-09-02 21:57:28 +1000

    Use styled components for Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
-interface Props {
-  children: React.ReactNode
-}
-
-const Header = ({ children }: Props) => {
-  return (
-    <header className="header">
-      { children }
-    </header>
-  )
-}
+const Header = styled.header`
+  border-bottom: 1px solid ${props => props.theme.borderColor.lightGrey};
+  box-shadow: 0 1px 4px rgb(0 0 0 / 10%);
+  padding: 0 30px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  
+  h1 {
+    font-size: ${props => props.theme.fontSize.base};
+    font-weight: 500;
+    margin: 0;
+  }
+`
 
 export default Header
