ea0cc684d036fc5c659951f9e364fd33a52fd777
Jeff Nielsen <jeff.nielsen@elmosoftware.com.au>
2022-09-02 21:56:39 +1000

    Use styled components for Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,15 +1,14 @@
 import React from "react";
+import styled from 'styled-components'
 
 interface Props {
   children: React.ReactNode
 }
 
-const Container = ({ children }: Props) => {
-  return (
-    <div className="container">
-      { children }
-    </div>
-  )
-}
+const Container = styled.div((props: Props) => ({
+  padding: '0 30px',
+  maxWidth: '1300px',
+  margin: '0 auto',
+}))
 
 export default Container
