6f72cd6bf2a009627b81d21a75cc3399a65cd37c
Jeff Nielsen <jeff.nielsen@elmosoftware.com.au>
2022-09-09 21:50:42 +1000

    Added props

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -11,13 +11,23 @@ interface Props {
 const Box = styled.span((props) => {
   return {
     flex: props.flex,
+
     paddingRight: props.paddingRight,
     paddingLeft: props.paddingLeft,
     paddingTop: props.paddingTop,
     paddingBottom: props.paddingBottom,
+    marginRight: props.marginRight,
+    marginLeft: props.marginLeft,
+    marginTop: props.marginTop,
+    marginBottom: props.marginBottom,
+
     width: props.width,
     maxWidth: props.maxWidth,
+    height: props.height,
+    maxHeight: props.maxHeight,
+
     overflowX: props.overflowX,
+    overflowY: props.overflowY,
   }
 })
 
