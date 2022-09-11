f59f46b596489ab982392a525027dbbbadadf006
Jeff Nielsen <jeff.nielsen@elmosoftware.com.au>
2022-09-11 17:18:21 +1000

    Added anchor styles

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,6 +5,17 @@ const Sidebar = styled.div((props) => ({
   borderRight: `1px solid ${props.theme.borderColor.lightGrey}`,
   width: '320px',
   minWidth: '320px',
+  a: {
+    textDecoration: 'none',
+    display: 'block',
+    padding: '10px 20px',
+    '&:hover': {
+      background: 'rgba(0,0,0,.02)',
+    },
+    '&.active': {
+      background: 'rgba(0,0,0,.05)',
+    }
+  }
 }))
 
 export default Sidebar
