5de918537c979be58c990f1f54d4f66b0e0ea854
Jeff Nielsen <jeff.nielsen@elmosoftware.com.au>
2022-12-13 08:57:46 +1100

    Fixed import

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import { ThemeProvider } from "@emotion/react";
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { GlobalStyle } from "./components/GlobalStyle";
+import routes from "./lib/routes";
+import theme from "./theme";
 import HomeScreen from "./screens/HomeScreen";
 import ReferenceScreen from "./screens/ReferenceScreen";
-import Layout from "./components/Layout";
 import StateProvider from "./state/stateProvider";
-import routes from "./lib/routes";
-import { ThemeProvider } from "@emotion/react";
-import theme from "./theme";
-import { GlobalStyle } from "./components/GlobalStyle";
 
 function App() {
   const pages = Object.keys(routes).map((route) => {
