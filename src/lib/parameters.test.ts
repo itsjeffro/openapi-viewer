4a86ef579ab1824db3cd889df616cf6756f9d793
Jeff Nielsen <jeff.nielsen@elmosoftware.com.au>
2023-03-30 13:43:44 +1100

    Added jest, added initial test

---
+++ src/lib/parameters.test.ts
@@ -0,0 +1,29 @@
+import { groupParams } from "./parameters";
+
+describe('parameters', () => {
+
+  it('should return pameters grouped in object by key', () => {
+    const parameters = [
+      {in: 'header', name: 'accept'},
+      {in: 'path', name: 'userId'},
+      {in: 'query', name: 'page'},
+    ];
+
+    const groups = groupParams(parameters);
+
+    const expected = {
+      headers: [
+        {in: 'header', name: 'accept'},
+      ],
+      paths: [
+        {in: 'path', name: 'userId'},
+      ],
+      queries: [
+        {in: 'query', name: 'page'},
+      ],
+    }
+
+    expect(groups).toEqual(expected);
+  })
+
+});
